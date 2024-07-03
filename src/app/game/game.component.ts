66c3a9a8ac649c6a33088a5770b2c30338f99efc
phischa <ph.git@mailbox.org>
2024-07-03 09:59:14 +0200

    card stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +11,21 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+    
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
 
   pickCard() {
     this.pickCardAnimation = true;

3b845c980012075c73b953c8a5ce3854ddda281f
phischa <ph.git@mailbox.org>
2024-07-05 17:09:46 +0200

    description card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../../app/player/player.component';
+import { GameInfoComponent } from '../../app/game-info/game-info.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule,],
+  imports: [CommonModule, PlayerComponent, GameInfoComponent, MatButtonModule, MatIconModule,],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -35,7 +40,7 @@ export class GameComponent implements OnInit {
 
   pickCard() {
     if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop() as string;;
+      this.currentCard = this.game.stack.pop() as string;
       this.pickCardAnimation = true;
       this.game.playedCards.push(this.currentCard);
       console.log('New Card:' + this.currentCard);
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-/* import { GameComponent } from '../../app/game/game.component'; */
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [CommonModule, /* GameComponent */],
+  imports: [CommonModule,],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
 export class PlayerComponent implements OnInit {
   @Input() name!: string;
+  @Input() playerActive: boolean = false;
 
   constructor() {
   }
